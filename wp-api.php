c670289b7a4a04cfa695090252605015a69cba70
Jeff Stieler <jeff.m.stieler@gmail.com>
2013-04-29 09:33:46 -0400

    Add test JSON output middleware.

--- wp-api.php
+++ wp-api.php
@@ -22,7 +22,6 @@ class API_Dispatcher {
 
 	public function __construct() {
 		//if requested url starts with api_base_url()
-		//add_action('wp_loaded', array($this, 'dispatch_api'));
 		if ( false !== strpos( $_SERVER['REQUEST_URI'], WP_API_BASE ) ) {
 			add_action( 'wp_loaded', array( $this, 'dispatch_api' ) );
 		}
@@ -31,20 +30,29 @@ class API_Dispatcher {
 	public function dispatch_api() {
 		//determine API version
 		require_once('lib/Slim/Slim.php');
+		require_once('JsonResponder.php');
 
 		\Slim\Slim::registerAutoloader();
 
 		$app = new \Slim\Slim();
 
-		$app->get(WP_API_BASE . '/v1/math/add/', function() {
-			echo 1;
+		$app->add(new \JsonResponder());
+
+		$app->get(WP_API_BASE . '/v1/math/add/', function() use ($app) {
+
+			$env = $app->environment();
+			$env['slim.response_object'] = array('result' => 1);
+
 		});
 
-		$app->get(WP_API_BASE . '/v2/math/add/', function() {
-			echo 2;
-		});
+		$app->get(WP_API_BASE . '/v2/math/add/:a/:b/', function($a, $b) use ($app) {
+			error_log(var_export($app->request()->params(), true));
+			echo $a + $b;
+		})->conditions(array('a'=>'\d+','b'=>'\d+'));
 
 		$app->run();
+
+		exit;
 	}
 
 }
