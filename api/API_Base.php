34522f0ad2c89b04f217fa7affc63139a2efb8e9
smccafferty <seanmcc@voceconnect.com>
2013-04-29 15:28:46 -0400

    added functional whitelist unit test

--- api/API_Base.php
+++ api/API_Base.php
@@ -28,11 +28,6 @@ abstract class API_Base {
 
 		// TODO: Check that version is specified
 
-		
-		
-		
-		
-		
 
 		// $this->app-run();
 	}
@@ -47,12 +42,20 @@ abstract class API_Base {
 	public function registerRoute( $method, $pattern, $callback ) {
 		$method = strtolower( $method );
 
-		$valid_methods = array( 'get', 'post', 'delete', 'put', 'head', 'options' );
+		$valid_methods = array(
+			'get',
+			'post',
+			'delete',
+			'put',
+			'head',
+			'options',
+		);
 
-		if ( !in_array( $method, $valid_methods ) )
+		if ( ! in_array( $method, $valid_methods ) ) {
 			return false;
+		}
 
-		return $this->app->$method( trailingslashit( WP_API_BASE ) . trailingslashit( 'v' . $this->version ) . $pattern, $callback);
+		return $this->app->$method( \trailingslashit( WP_API_BASE ) . \trailingslashit( 'v' . $this->version ) . $pattern, $callback );
 	}
 
 }
--- tests/API_BaseTest.php
+++ tests/API_BaseTest.php
@@ -1,11 +1,42 @@
 <?php
 
+global $wp, $wp_the_query, $wp_query;
+
+define("WP_USE_THEMES", false);
+require dirname( __FILE__ ) . '/../../../../wp-blog-header.php';
+require dirname( __FILE__ ) . '/../api/API_Base.php';
+require dirname( __FILE__ ) . '/../lib/Slim/Slim/Slim.php';
+
+class Version_API_Test_1 extends \WP_JSON_API\API_Base {
+
+	protected $version = '1';
+	
+}
+
+class Version_API_Test_2 extends \WP_JSON_API\API_Base {
+
+	protected $version = '2';
+
+}
+
 class API_BaseTest extends PHPUnit_Framework_TestCase {
 
-	public function testRouteRequest() {
+	public function setUp() {
 
-		$this->assertEquals(1, 1);
+		\Slim\Slim::registerAutoloader();
+    }
 
+	public function testRegisterRouteWhitelist() {
+
+		$slim = new \Slim\Slim();
+
+		$apitest1 = new Version_API_Test_1( $slim );
+
+		$test = $apitest1->registerRoute( 'get', 'abc', function(){} );
+		$this->assertInstanceOf( '\Slim\Route', $test );
+
+		$test2 = $apitest1->registerRoute( 'yum', 'abc', function(){} );
+		$this->assertFalse( $test2 );
 	}
 
 }
