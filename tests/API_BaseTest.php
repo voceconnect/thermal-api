6ac8f0f8633f5f809a03cb5824adadc1825c77e5
smccafferty <seanmcc@voceconnect.com>
2013-04-29 15:42:48 -0400

    adding tests for api version

--- tests/API_BaseTest.php
+++ tests/API_BaseTest.php
@@ -2,10 +2,11 @@
 
 global $wp, $wp_the_query, $wp_query;
 
-define("WP_USE_THEMES", false);
-require dirname( __FILE__ ) . '/../../../../wp-blog-header.php';
-require dirname( __FILE__ ) . '/../api/API_Base.php';
-require dirname( __FILE__ ) . '/../lib/Slim/Slim/Slim.php';
+define( 'WP_API_BASE', 'api' );
+define( 'WP_USE_THEMES', false );
+require_once( dirname( __FILE__ ) . '/../../../../wp-blog-header.php' );
+require_once( dirname( __FILE__ ) . '/../api/API_Base.php' );
+require_once( dirname( __FILE__ ) . '/../lib/Slim/Slim/Slim.php' );
 
 class Version_API_Test_1 extends \WP_JSON_API\API_Base {
 
@@ -34,9 +35,24 @@ class API_BaseTest extends PHPUnit_Framework_TestCase {
 
 		$test = $apitest1->registerRoute( 'get', 'abc', function(){} );
 		$this->assertInstanceOf( '\Slim\Route', $test );
+		$this->assertContains( 'GET', $test->getHttpMethods() );
 
 		$test2 = $apitest1->registerRoute( 'yum', 'abc', function(){} );
 		$this->assertFalse( $test2 );
+	}
+
+	public function testAPIVersion() {
+		$slim = new \Slim\Slim();
+
+		$apitest1 = new Version_API_Test_1( $slim );
+
+		$test = $apitest1->registerRoute( 'get', 'abc', function(){} );
+		$this->assertEquals( 'api/v1/abc', $test->getPattern() );
+
+		$apitest1 = new Version_API_Test_2( $slim );
+
+		$test = $apitest1->registerRoute( 'get', 'abc', function(){} );
+		$this->assertEquals( 'api/v2/abc', $test->getPattern() );
 	}
 
 }
