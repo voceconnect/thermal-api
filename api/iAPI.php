48da57fd33446ec9a3897fbd0c8745a8de68e60b
Gary Smirny <banderon@gmail.com>
2013-04-29 09:44:15 -0400

    killed symfony

--- api/iAPI.php
+++ api/iAPI.php
@@ -1,7 +1,5 @@
 <?php
 namespace WP_JSON_API;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 
 interface iAPI {
 	/**
--- api/v1/API.php
+++ api/v1/API.php
@@ -2,9 +2,6 @@
 
 namespace WP_JSON_API;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-
 class API implements iAPI {
 
 	public function getVersion() {
@@ -14,8 +11,6 @@ class API implements iAPI {
 	/**
 	 * Routes the request to the correct handler and fills the response accordingly
 	 * 
-	 * @param \Symfony\Component\HttpFoundation\Request $request
-	 * @param \Symfony\Component\HttpFoundation\Response $response
 	 */
 	public function handleRequest( Request $request, Response $response ) {
 		throw new Exception( "Method Not Implemented" );
