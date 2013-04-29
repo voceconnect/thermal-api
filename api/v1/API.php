a2a15417777358281369e565a5c76f531fcb002e
John Ciacia <john.ciacia@gmail.com>
2013-04-29 10:45:42 -0400

    passing in slim object

--- api/v1/API.php
+++ api/v1/API.php
@@ -7,7 +7,7 @@ class API extends API_Base {
 	protected $version = '1';
 
 	public function __construct() {
-		parent::__construct( $slim );
+		parent::__construct( new \Slim\Slim() );
 		$this->registerRoute( 'GET', WP_API_BASE . '/users/:id', array( __CLASS__, 'users' ) );
 		$this->registerRoute( 'GET', WP_API_BASE . '/posts', array( __CLASS__, 'posts' ) );
 	}
