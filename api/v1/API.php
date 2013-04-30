bafb69800194ef22e4ecd434439b411f6ed6c6e7
Gary Smirny <banderon@gmail.com>
2013-04-30 11:08:27 -0400

    added post formatting

--- api/v1/API.php
+++ api/v1/API.php
@@ -15,10 +15,24 @@ class APIv1 extends API_Base {
 	}
 
 	public static function get_users( $id = null ) {
+		$users = array();
+		if ( $id ) {
+			$users[] = self::format_user( get_user_by( $id ) );
+			return compact( 'users' );
+		}
+
 		return WP_API_BASE . '/users/' . $id;
 	}
 
 	public static function get_posts( $id = null ) {
+		$posts = array();
+		if ( $id ) {
+			$posts[] = self::format_post( get_post( $id ) );
+			return compact( 'posts' );
+		}
+
+		// TODO: Get posts and loop through, setting to $posts[]
+
 		return WP_API_BASE . '/posts/' . $id;
 	}
 
@@ -29,4 +43,46 @@ class APIv1 extends API_Base {
 	public static function get_terms( $name, $term_id = null ) {
 		return WP_API_BASE . '/taxonomies/' . $name . '/terms/' . $term_id;
 	}
+
+	/**
+	 * Format post data
+	 * @param \WP_Post $post
+	 * @return Array Formatted post data
+	 */
+	public static function format_post( \WP_Post $post ) {
+		$GLOBALS['post'] = $post;
+		setup_postdata( $post );
+		$data = array(
+			'id' => $post->ID,
+			'id_str' => (string)$post->ID,
+			'permalink' => \get_permalink( $post ),
+			'parent' => $post->post_parent,
+			'parent_str' => (string)$post->post_parent,
+			'date' => \get_the_time( 'c', $post ),
+			'modified' => \get_post_modified_time( 'c', true, $post ),
+			'status' => $post->post_status,
+			'comment_status' => $post->comment_status,
+			'comment_count' => (int)$post->comment_count,
+			'menu_order' => $post->menu_order,
+			'title' => $post->post_title,
+			'name' => $post->post_name,
+			'excerpt_raw' => $post->post_excerpt,
+			'excerpt' => \apply_filters( 'the_excerpt', \get_the_excerpt() ),
+			'content_raw' => $post->post_content,
+			'content' => \apply_filters( 'the_content', \get_the_content() ),
+			'content_filtered' => $post->post_content_filtered,
+			'mime_type' => $post->post_mime_type,
+		);
+		wp_reset_postdata();
+
+		return $data;
+	}
+
+	/**
+	 * Format user data
+	 * @param \WP_User $user
+	 * @return Array Formatted user data
+	 */
+	public static function format_user( \WP_User $user ) {}
+
 }
