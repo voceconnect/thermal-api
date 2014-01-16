75351f4927428bd152eda42f4aca57fdeada0a5a
= <mpretty@voceconnect.com>
2014-01-16 09:12:43 -0500

    adding composer install to travis install

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -73,6 +73,10 @@ install_db() {
         mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
 }
 
+install_dependencies() {
+        composer install
+}
+
 install_wp
 install_test_suite
 install_db
