6aea139ca217a1269653835f0e822a8c0b4f271f
Curtis Loisel <curtis@voceconnect.com>
2014-07-01 14:15:38 -0400

    Adding cap deploy files and bumping up version

---
+++ Capfile
@@ -0,0 +1,8 @@
+# Includes default deployment tasks
+require 'capistrano/deploy'
+
+# Voce Platforms specific tasks and support
+require 'capistrano/platforms/setup'
+
+# Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
+Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
---
+++ config/deploy/production.rb
@@ -0,0 +1,21 @@
+set :application, 'thermal-api'
+set :repo_url, "git@github.com:voceconnect/#{fetch(:application)}.git"
+
+set :scm, 'git-to-svn'
+set :type, 'plugin'
+
+set :svn_repository, "http://plugins.svn.wordpress.org/#{fetch(:application)}/"
+set :svn_deploy_to, "trunk"
+
+## additional files to exclude from committing to svn
+set :build_files, (
+  fetch(:build_files) << %w{
+    phpunit.xml*
+  }
+).flatten
+
+set :build_folders, (
+  fetch(:build_folders) << %w{
+  	config
+  }
+).flatten
