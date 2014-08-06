df4cb50ee716a18d9e2e0975e3c1ef4d8c6e4292
Curtis Loisel <curtis@voceconnect.com>
2014-08-06 11:16:44 -0400

    Adding basic gallery test

--- tests/v1/controllers/test-Posts.php
+++ tests/v1/controllers/test-Posts.php
@@ -586,4 +586,24 @@ class PostsControllerTest extends APITestCase {
 		$this->assertObjectNotHasAttribute( 'found', $data );
 	}
 
+	public function testPostMetaGallery() {
+		$post_content = 'Lorem Ipsum [gallery]';
+		$post_args = array( 'post_content' => $post_content );
+		$post_images = array( '100x200.png', '100x300.png', '100x400.png' );
+		$post_data = self::_insert_post( $post_args, $post_images );
+		$post_id = $post_data['post_id'];
+		$attachment_ids = $post_data['attachment_ids'];
+
+		list($status, $headers, $body) = $this->_getResponse( array(
+			'REQUEST_METHOD' => 'GET',
+			'PATH_INFO' => Voce\Thermal\get_api_base() . 'v1/posts/' . $post_id,
+			'QUERY_STRING' => '',
+		) );		
+
+		$data = json_decode( $body );
+
+		$this->assertAttributeNotEmpty( 'gallery', $data->meta );
+		$this->assertEquals( $attachment_ids, $data->meta->gallery[0]->ids );
+	}
+
 }
