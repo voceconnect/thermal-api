58854e6ac229acef407c1a640861f876b366f6a5
Curtis Loisel <curtis@voceconnect.com>
2014-08-07 09:27:07 -0400

    Ids attribute test

--- tests/v1/controllers/test-Posts.php
+++ tests/v1/controllers/test-Posts.php
@@ -677,4 +677,32 @@ class PostsControllerTest extends APITestCase {
 		$this->assertEquals( $attachment_ids, $data->meta->gallery[0]->ids );
 	}
 
+	public function testGetPostGalleryIDs() {
+		$post_content = 'Lorem Ipsum';
+		$post_args = array( 'post_content' => $post_content );
+		$post_images = array( '100x200.png', '100x300.png', '100x400.png' );
+		$post_data = self::_insert_post( $post_args, $post_images );
+		$post_id = $post_data['post_id'];
+		$attachment_ids = $post_data['attachment_ids'];
+
+		array_shift($attachment_ids);
+
+		self::_insert_post(
+			array(
+				'ID'           => $post_id,
+				'post_content' => sprintf( '[gallery ids="%s"]', implode(',', $attachment_ids) )
+			)
+		);
+
+		list($status, $headers, $body) = $this->_getResponse( array(
+			'REQUEST_METHOD' => 'GET',
+			'PATH_INFO' => Voce\Thermal\get_api_base() . 'v1/posts/' . $post_id,
+			'QUERY_STRING' => '',
+		) );
+
+		$data = json_decode( $body );
+
+		$this->assertEquals( $attachment_ids, $data->meta->gallery[0]->ids );
+	}
+
 }
